f8545beae273ec19adfdad4b52a1cd5c16626df6
Dario Castellano <>
2019-11-11 12:47:37 +0100

    firebase env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase : {
+    apiKey: 'AIzaSyAExQr_1sD9mHP5-FAKBwrtIpRL_TJJTjU',
+    authDomain: 'torneo-acg.firebaseapp.com',
+    databaseURL: 'https://torneo-acg.firebaseio.com',
+    projectId: 'torneo-acg',
+    storageBucket: 'torneo-acg.appspot.com',
+    messagingSenderId: '1086638444901',
+    appId: '1:1086638444901:web:ff68f80955bd6ccde4518c',
+    measurementId: 'G-468QVHFRZH'
+  }
 };
