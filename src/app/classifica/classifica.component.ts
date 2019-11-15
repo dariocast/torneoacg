09942780dbb1e1ff9f522fb3473f74c5dc5667d9
Dario Castellano <dariocastellano18@gmail.com>
2019-11-15 01:56:39 +0100

    finish

--- src/app/classifica/classifica.component.ts
+++ src/app/classifica/classifica.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {AngularFirestore, AngularFirestoreCollection} from '@angular/fire/firestore';
 import {Squadra} from '../models/squadra.model';
 import {Observable} from 'rxjs';
+import {DataSource} from '@angular/cdk/table';
 
 @Component({
   selector: 'app-classifica',
@@ -9,16 +10,40 @@ import {Observable} from 'rxjs';
   styleUrls: ['./classifica.component.css']
 })
 export class ClassificaComponent implements OnInit {
+  displayedColumns: string[] = ['squadra', 'pg', 'v', 'p', 's', 'gf', 'gs', 'dg', 'pt', 'ultime'];
+  public gironeA: SquadreDataSource;
+  public gironeC: SquadreDataSource;
+  public gironeB: SquadreDataSource;
+  public gironeAF: SquadreDataSource;
+  public gironeBF: SquadreDataSource;
 
-  private squadraCollection: AngularFirestoreCollection<Squadra>;
-  squadre: Observable<Squadra[]>;
+
 
   constructor(private afs: AngularFirestore) {
-    this.squadraCollection = this.afs.collection<Squadra>('squadre');
-    this.squadre = this.squadraCollection.valueChanges();
+    this.gironeA = new SquadreDataSource(afs, 'a');
+    this.gironeB = new SquadreDataSource(afs, 'b');
+    this.gironeC = new SquadreDataSource(afs, 'c');
+    this.gironeAF = new SquadreDataSource(afs, 'af');
+    this.gironeBF = new SquadreDataSource(afs, 'bf');
   }
 
   ngOnInit() {
   }
 
+}
+
+export class SquadreDataSource extends DataSource<any> {
+
+  constructor(private afs: AngularFirestore, private girone: string) {
+    super();
+  }
+
+  connect() {
+    return this.afs.collection<Squadra>('squadre', ref =>
+      ref.where('girone', '==', this.girone).orderBy('pt')).valueChanges();
+  }
+
+  disconnect() {
+
+  }
 }
--- src/app/models/squadra.model.ts
+++ src/app/models/squadra.model.ts
@@ -2,4 +2,12 @@ export interface Squadra {
   nome: string;
   logo: string;
   girone: string;
+  pg: number;
+  v: number;
+  p: number;
+  s: number;
+  gf: number;
+  gs: number;
+  pt: number;
+  ultime: [];
 }
